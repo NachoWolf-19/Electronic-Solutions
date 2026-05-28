6574e7970ee61a554a9020acca429d5251fc326f
NachoWolf-19 <257172180+NachoWolf-19@users.noreply.github.com>
2026-05-28 17:31:59 -0500

    bd

--- 5_APLICACION/ElecSoluDB/ElecSoluDB - Script.sql
+++ 5_APLICACION/ElecSoluDB/ElecSoluDB - Script.sql
@@ -20,7 +20,7 @@ CREATE TABLE clientes(
     CONSTRAINT ch_clienteNumero 	CHECK (clienteNumero REGEXP '^[0-9]{9}$' OR clienteNumero = 'Sin Numero'),
     CONSTRAINT ch_clienteEmail 		CHECK (clienteEmail REGEXP '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$' OR clienteEmail = 'Sin Email'),
     CONSTRAINT ch_clienteEstado 	CHECK (clienteEstado IN ('Activo', 'Borrado')),
-    CONSTRAINT uq_clienteDNI 		UNIQUE (clienteEstado)
+    CONSTRAINT uq_clienteDNI 		UNIQUE (clienteDNI)
 );
 
 -- =================================== Tabla Repuestos
@@ -30,7 +30,7 @@ CREATE TABLE repuestos(
     repuestoStock INT NOT NULL,
     repuestoPrecio DECIMAL(10,2),
     CONSTRAINT pk_repuestos PRIMARY KEY (repuestoId),
-    CONSTRAINT ch_repuestoNombre CHECK (LENGTH(TRIM(clienteNombre)) > 0),
+    CONSTRAINT ch_repuestoNombre CHECK (LENGTH(TRIM(repuestoNombre)) > 0),
     CONSTRAINT ch_repuestoStock CHECK (repuestoStock >= 0),
     CONSTRAINT ch_repuestoPrecio CHECK (repuestoPrecio >= 0),
     CONSTRAINT uq_repuestoNombre UNIQUE (repuestoNombre)
@@ -45,6 +45,21 @@ CREATE TABLE categorias(
     CONSTRAINT uq_categoriaNombre 	UNIQUE (categoriaNombre)
 );
 
+-- =================================== Tabla Tecnicos
+CREATE TABLE tecnicos(
+	tecnicoId INT AUTO_INCREMENT,
+    tecnicoNombre VARCHAR(50) NOT NULL,
+    tecnicoApellido VARCHAR(50) NOT NULL,
+    categoriaId INT NOT NULL,
+    tecnicoEstado VARCHAR(15) DEFAULT 'Activo',
+    CONSTRAINT pk_tecnicos 				PRIMARY KEY (tecnicoId),
+    CONSTRAINT fk_tecnicos_categorias 	FOREIGN KEY (categoriaId) REFERENCES categorias(categoriaId),
+    CONSTRAINT ch_tecnicoNombre 		CHECK (tecnicoNombre REGEXP '^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$' AND LENGTH(TRIM(tecnicoNombre)) > 0),
+    CONSTRAINT ch_tecnicoApellido 		CHECK (tecnicoApellido REGEXP '^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$' AND LENGTH(TRIM(tecnicoApellido)) > 0),
+    CONSTRAINT ch_tecnicoEstado			CHECK (tecnicoEstado IN ('Activo', 'Vacaciones', 'Retirado'))
+);
+
+-- =================================== Tabla Ordenes
 CREATE TABLE ordenes(
 	ordenId INT AUTO_INCREMENT,
     ordenServicio VARCHAR(15) NOT NULL,
@@ -57,9 +72,22 @@ CREATE TABLE ordenes(
     ordenFechaRegistro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     ordenFechaEntrega DATE NOT NULL,
     ordenGarantiaMeses INT NOT NULL,
+    ordenPrecio DECIMAL(10,2) NOT NULL,
     ordenEstado VARCHAR (15) NOT NULL DEFAULT 'Registrado',
     CONSTRAINT pk_ordenes PRIMARY KEY (ordenId),
     CONSTRAINT fk_ordenes_clientes FOREIGN KEY (clienteId) REFERENCES clientes(clienteId),
     CONSTRAINT fk_ordenes_categoria FOREIGN KEY (categoriaId) REFERENCES categorias(categoriaId),
     CONSTRAINT ch_ordenServicio CHECK (ordenServicio IN ('Mantenimiento', 'Reparacion')) 
+);
+
+CREATE TABLE detalleOrdenes(
+    ordenId          		INT NOT NULL,
+    repuestoId       		INT NOT NULL,
+    repuestoCantidad 		INT NOT NULL,
+    repuestoPrecioUnitario  DECIMAL(10,2) NOT NULL,
+    CONSTRAINT pk_detalles_ordenes PRIMARY KEY (ordenid, repuestoid),
+    CONSTRAINT fk_detalles_ordenes_ordenes FOREIGN KEY (ordenid) REFERENCES ordenes(ordenid),
+    CONSTRAINT fk_detalles_ordenes_repuestos FOREIGN KEY (repuestoid) REFERENCES repuestos(repuestoid),
+    CONSTRAINT ch_detalle_cantidad CHECK (cantidad > 0),
+    CONSTRAINT ch_detalle_precio CHECK (preciounitario >= 0)
 );
